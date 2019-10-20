7e52b25d4f6de749f06216c7776cb4f3568b2e03
raythurman2386 <raymondthurman5@gmail.com>
2019-10-20 10:38:57 -0400

    ....changed to V-8 engine in app....

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from './components/Header'
+import AddedFeatures from './components/AddedFeatures'
+import AdditionalFeatures from './components/AdditionalFeatures'
+import Total from './components/Total'
 
 const App = () => {
   const state = {
@@ -16,33 +16,33 @@ const App = () => {
       features: []
     },
     additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 1, name: 'V-8 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
       { id: 4, name: 'Rear spoiler', price: 250 }
     ]
-  };
+  }
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
-  };
+  }
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-  };
+  }
 
   return (
-    <div className="boxes">
-      <div className="box">
+    <div className='boxes'>
+      <div className='box'>
         <Header car={state.car} />
         <AddedFeatures car={state.car} />
       </div>
-      <div className="box">
+      <div className='box'>
         <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
