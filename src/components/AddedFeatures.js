7761a30b547f45026439df1111aafd5d50e276a9
raythurman2386 <raymondthurman5@gmail.com>
2019-10-21 22:29:41 -0400

    useSelector hook

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import { removeFeature } from '../actions'
 
 import AddedFeature from './AddedFeature'
 
 const AddedFeatures = props => {
+  const car = useSelector(state => state.car)
+
   const removeFeature = item => {
     // dispatch an action here to remove an item
     props.removeFeature(item)
@@ -13,9 +15,9 @@ const AddedFeatures = props => {
   return (
     <div className='content'>
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {car.features.length ? (
         <ol type='1'>
-          {props.car.features.map(item => (
+          {car.features.map(item => (
             <AddedFeature
               key={item.id}
               removeFeature={removeFeature}
@@ -30,13 +32,7 @@ const AddedFeatures = props => {
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    car: state.car
-  }
-}
-
 export default connect(
-  mapStateToProps,
+  null,
   { removeFeature }
 )(AddedFeatures)
